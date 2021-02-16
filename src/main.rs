5c6120bee7a2696cad57f951de4c8aca627a63cf
Sabaniki <sawada13@mccc.jp>
2021-02-16 19:53:44 +0900

    エラー処理用のクロージャを定義

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ mod udp;
 mod tcp;
 mod util;
 
-use std::env;
-use util::{app, arg_types};
-
 #[macro_use]
 extern crate log;
 
+use std::env;
+use util::{app, arg_types};
 use arg_types::{Protocol, StartAs, get_args};
 use log::error;
 
@@ -16,17 +15,18 @@ fn main() {
     env_logger::init();
     let (protocol_type, start_as, unchecked_address) = get_args();
     let address_str = unchecked_address.as_str();
+    let print_error = | e: failure::Error | {error!("{}", e)};
 
     match protocol_type {
         Protocol::Tcp => {
             match start_as {
                 StartAs::Server => {
                     tcp::server::serve(address_str)
-                        .unwrap_or_else(|error| error!("{}", error));
+                        .unwrap_or_else(print_error);
                 }
                 StartAs::Client => {
                     tcp::client::connect(address_str)
-                        .unwrap_or_else(|error| error!("{}", error));
+                        .unwrap_or_else(print_error);
                 }
             }
         }
@@ -34,11 +34,11 @@ fn main() {
             match start_as {
                 StartAs::Server => {
                     udp::server::serve(address_str)
-                        .unwrap_or_else(|error| error!("{}", error));
+                        .unwrap_or_else(print_error);
                 }
                 StartAs::Client => {
                     udp::client::communicate(address_str)
-                        .unwrap_or_else(|error| error!("{}", error));
+                        .unwrap_or_else(print_error);
                 }
             }
         }
